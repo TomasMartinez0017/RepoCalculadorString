61dac6131379626057dea3e41b3cb585ce185a41
TomasMartinez0017 <tomasmartinez0017@gmail.com>
2021-04-06 09:55:12 -0300

    Subo los tests

---
+++ CalculadoraStrings/CalculadoraStringsTest/CalculadoraTest.cs
@@ -0,0 +1,44 @@
+﻿using CalculadoraStrings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculadoraStringsTest
+{
+    [TestClass]
+    public class CalculadoraTest
+    {
+        [TestMethod]
+        public void SumarStringVacio()
+        {
+            //Arrange
+            Calculadora calc = new Calculadora();
+
+            //Act
+            int suma = calc.Suma("");
+
+            //Assert
+            //1: expected
+            //2: actual
+            Assert.AreEqual(0, suma);
+        }
+        [TestMethod]
+        public void SumarStringsSinComa()
+        {
+            //Arrange
+            Calculadora calc = new Calculadora();
+            //Act
+            int suma = calc.Suma("1");
+            //Assert
+            Assert.AreEqual(1, suma);
+        }
+        [TestMethod]
+        public void SumarStringsConComaYDosNumeros()
+        {
+            //Arrange
+            Calculadora calc = new Calculadora();
+            //Act
+            int suma = calc.Suma("4,6");
+            //Assert
+            Assert.AreEqual(10, suma);
+        }
+    }
+}
---
+++ CalculadoraStrings/CalculadoraStringsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculadoraStringsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculadoraStringsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0b6ba56a-086f-4cce-8f38-cbadf3f9c125")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
